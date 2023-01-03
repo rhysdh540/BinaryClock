23dbb709c280bf463337761a328a2b48d7972de8
rhysdh540 <rhys_dehaan@ryecountryday.org>
2023-01-03 10:37:07 -0500

    try to use windows look and feel

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,15 @@ public class Main {
             System.setProperty("apple.awt.application.name", "BinaryClock");
             System.setProperty("apple.awt.application.appearance", "system");
         }
+        // uncomment to print the installed L&Fs
         //for(UIManager.LookAndFeelInfo l : UIManager.getInstalledLookAndFeels())
         //    System.out.println(l.getClassName());
         try {
-            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-        }catch(Exception e){
+            if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            } else
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch(Exception e) {
             e.printStackTrace(System.err);
         }
         new GUI();

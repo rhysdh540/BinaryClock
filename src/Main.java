d503d8be6f27725ce2b63174880f3f0fb99b8f1f
SamSN <samuelsahnixon@gmail.com>
2023-01-04 14:33:14 -0500

    Minor Spelling Mistake Fix #2

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
             else if(System.getProperty("os.name").toLowerCase().contains("windows"))
                 UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); // windows default L&F
             else
-                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel"); // acceptable alternative (doesn't matter in macos since we're using the system menubar there)
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel"); // acceptable alternative (doesn't matter in macOS since we're using the system menubar there)
         } catch (Exception e) {
             System.err.println("Error with Loading Custom UI!");
             e.printStackTrace(System.err);
